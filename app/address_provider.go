329b8642bd0f035a77603aad7279af0d39d21408
Taeguk Kwon <xornrbboy@gmail.com>
2024-11-03 19:41:25 +0700

    fix: should not verify fee recipient address

--- app/address_provider.go
+++ app/address_provider.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	evmengtypes "github.com/omni-network/omni/octane/evmengine/types"
 )
@@ -24,9 +23,5 @@ func (s ValidatorAddressProvider) LocalFeeRecipient() common.Address {
 }
 
 func (s ValidatorAddressProvider) VerifyFeeRecipient(address common.Address) error {
-	if address != s.Addr {
-		return fmt.Errorf("fee recipient not the burn address. addr: %s", address.Hex())
-	}
-
 	return nil
 }
