fcd022b3e7aeaf881d1c147545cf0ba0a02bbf74
Taeguk Kwon <xornrbboy@gmail.com>
2025-03-29 08:45:16 +0900

    feat(x/evmvalidator): use telemetry for end blocker

--- x/evmvalidator/keeper/abci.go
+++ x/evmvalidator/keeper/abci.go
@@ -2,11 +2,16 @@ package keeper
 
 import (
 	abci "github.com/cometbft/cometbft/abci/types"
+	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/mitosis-org/chain/x/evmvalidator/types"
 )
 
 // EndBlocker is called at the end of every block
 func (k Keeper) EndBlocker(ctx sdk.Context) ([]abci.ValidatorUpdate, error) {
+	defer telemetry.ModuleMeasureSince(types.ModuleName, telemetry.Now(), telemetry.MetricKeyEndBlocker)
+
 	// Process matured withdrawals
 	if err := k.ProcessMaturedWithdrawals(ctx); err != nil {
 		return nil, err
